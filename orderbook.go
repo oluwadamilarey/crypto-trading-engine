46a5a425bc8580403d7170e7e0e482374623ab1f
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2023-11-24 14:14:35 +0100

    feat: ielemented add and place method for orderbook mutations

--- orderbook.go
+++ orderbook.go
@@ -1,6 +1,16 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+type Match struct {
+	Ask        *Order
+	Bid        *Order
+	SizeFilled float64
+	Price      float64
+}
 
 type Order struct {
 	Size      float64
@@ -15,6 +25,10 @@ func NewOrder(bid bool, size float64) *Order {
 		Bid:       bid,
 		Timestamp: time.Now().UnixNano(),
 	}
+}
+
+func (o *Order) String() string {
+	return fmt.Sprintf("[size: %.2f]", o.Size)
 }
 
 type Limit struct {
@@ -36,7 +50,66 @@ func (l *Limit) AddOrder(o *Order) {
 	l.TotalVolume += o.Size
 }
 
+func (l *Limit) DeleteOrder(o *Order) {
+	for i := 0; i < len(l.Orders); i++ {
+		if l.Orders[i] == o {
+			l.Orders[i] = l.Orders[len(l.Orders)-1]
+			l.Orders = l.Orders[:len(l.Orders)-1]
+		}
+	}
+
+	o.Limit = nil
+	l.TotalVolume -= o.Size
+
+	//TODO: resort the whole resting orders
+}
+
 type OrderBook struct {
 	Asks []*Limit
 	Bids []*Limit
+
+	AskLimits map[float64]*Limit
+	BidLimits map[float64]*Limit
+}
+
+func NewOrderBook() *OrderBook {
+	return &OrderBook{
+		Asks:      []*Limit{},
+		Bids:      []*Limit{},
+		AskLimits: make(map[float64]*Limit),
+		BidLimits: make(map[float64]*Limit),
+	}
+}
+
+func (ob *OrderBook) PlaceOrder(price float64, o *Order) []Match {
+	// 1 try to match the order
+	// matching logic
+
+	//2. add the rest of the order to the books
+	if o.Size > 0.0 {
+		ob.add(price, o)
+	}
+
+	return []Match{}
+}
+
+func (ob *OrderBook) add(price float64, o *Order) {
+	var limit *Limit
+
+	if o.Bid {
+		limit = ob.BidLimits[price]
+	} else {
+		limit = ob.AskLimits[price]
+	}
+
+	if limit == nil {
+		limit = NewLimit(price)
+		if o.Bid {
+			ob.Asks = append(ob.Asks, limit)
+			ob.BidLimits[price] = limit
+		} else {
+			ob.Asks = append(ob.Bids, limit)
+			ob.AskLimits[price] = limit
+		}
+	}
 }
--- orderbook_test.go
+++ orderbook_test.go
@@ -5,13 +5,31 @@ import (
 	"testing"
 )
 
-func TestOrderBook(t *testing.T) {
+// func TestOrderBook(t *testing.T) {
 
-}
+// }
 
 func TestLimit(t *testing.T) {
 	l := NewLimit(10_000)
+	buyOrderA := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 8)
+	buyOrderC := NewOrder(true, 10)
+
+	l.AddOrder(buyOrderA)
+	l.AddOrder(buyOrderB)
+	l.AddOrder(buyOrderC)
+
+	l.DeleteOrder(buyOrderB)
+
 	buyOrder := NewOrder(true, 5)
 	l.AddOrder(buyOrder)
 	fmt.Println(l)
+}
+
+func TestOrderBook(t *testing.T) {
+	ob := NewOrderBook()
+	buyOrder := NewOrder(true, 10)
+	ob.PlaceOrder(10_000, buyOrder)
+
+	fmt.Printf("%+v", ob.Bids[0])
 }
