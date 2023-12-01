35d21dabc0e38c412b957a2e832c298c070ab572
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2023-12-01 01:38:46 +0100

    updates:began working on market placing mechanism -> defined a placemarket order method

--- orderbook.go
+++ orderbook.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"time"
 )
 
@@ -19,6 +20,12 @@ type Order struct {
 	Timestamp int64
 }
 
+type Orders []*Order
+
+func (o Orders) Len() int           { return len(o) }
+func (o Orders) Swap(i, j int)      { o[i], o[j] = o[j], o[i] }
+func (o Orders) Less(i, j int) bool { return o[i].Timestamp < o[j].Timestamp }
+
 func NewOrder(bid bool, size float64) *Order {
 	return &Order{
 		Size:      size,
@@ -31,11 +38,29 @@ func (o *Order) String() string {
 	return fmt.Sprintf("[size: %.2f]", o.Size)
 }
 
+func (o *Order) isFilled() bool {
+	return o.Size == 0.0
+}
+
 type Limit struct {
 	Price       float64
-	Orders      []*Order
+	Orders      Orders
 	TotalVolume float64
 }
+
+type Limits []*Limit
+
+type ByBestAsk struct{ Limits }
+
+func (a ByBestAsk) Len() int           { return len(a.Limits) }
+func (a ByBestAsk) Swap(i, j int)      { a.Limits[i], a.Limits[j] = a.Limits[j], a.Limits[i] }
+func (a ByBestAsk) Less(i, j int) bool { return a.Limits[i].Price < a.Limits[j].Price }
+
+type ByBestBid struct{ Limits }
+
+func (b ByBestBid) Len() int           { return len(b.Limits) }
+func (b ByBestBid) Swap(i, j int)      { b.Limits[i], b.Limits[j] = b.Limits[j], b.Limits[i] }
+func (a ByBestBid) Less(i, j int) bool { return a.Limits[i].Price > a.Limits[j].Price }
 
 func NewLimit(price float64) *Limit {
 	return &Limit{
@@ -62,11 +87,60 @@ func (l *Limit) DeleteOrder(o *Order) {
 	l.TotalVolume -= o.Size
 
 	//TODO: resort the whole resting orders
+	sort.Sort(l.Orders)
+}
+
+func (l *Limit) Fill(o *Order) []Match {
+	matches := []Match{}
+
+	for _, order := range l.Orders {
+		match := l.fillOrder(order, o)
+		matches = append(matches, match)
+
+		if o.isFilled() {
+			break
+		}
+	}
+
+	return matches
+}
+
+// TODO: Add more context to fillorder function, preferably with chatGPT
+func (l *Limit) fillOrder(a, b *Order) Match {
+	var (
+		bid        *Order
+		ask        *Order
+		sizeFilled float64
+	)
+
+	if a.Bid {
+		bid = a
+		ask = b
+	} else {
+		bid = b
+		ask = a
+	}
+
+	if a.Size > b.Size {
+		a.Size -= b.Size
+		sizeFilled = b.Size
+		b.Size = 0.0
+	} else {
+		b.Size -= a.Size
+		sizeFilled = a.Size
+	}
+
+	return Match{
+		Bid:        bid,
+		Ask:        ask,
+		SizeFilled: sizeFilled,
+		Price:      l.Price,
+	}
 }
 
 type OrderBook struct {
-	Asks []*Limit
-	Bids []*Limit
+	asks []*Limit
+	bids []*Limit
 
 	AskLimits map[float64]*Limit
 	BidLimits map[float64]*Limit
@@ -74,26 +148,32 @@ type OrderBook struct {
 
 func NewOrderBook() *OrderBook {
 	return &OrderBook{
-		Asks:      []*Limit{},
-		Bids:      []*Limit{},
+		asks:      []*Limit{},
+		bids:      []*Limit{},
 		AskLimits: make(map[float64]*Limit),
 		BidLimits: make(map[float64]*Limit),
 	}
 }
 
-func (ob *OrderBook) PlaceOrder(price float64, o *Order) []Match {
-	// 1 try to match the order
-	// matching logic
+func (ob *OrderBook) PlaceMarketOrder(o *Order) []Match {
+	matches := []Match{}
 
-	//2. add the rest of the order to the books
-	if o.Size > 0.0 {
-		ob.add(price, o)
+	if o.Bid {
+		if o.Size > ob.AskTotalVolume() {
+			panic(fmt.Errorf("Not enough volume [%.2f] for market order [size: %.2f]", ob.AskTotalVolume(), o.Size))
+		}
+		for _, limit := range ob.Asks() {
+			limitMatches := limit.Fill(o)
+			matches = append(matches, limitMatches...)
+		}
+	} else {
+		fmt.Printf("coool")
 	}
 
-	return []Match{}
+	return matches
 }
 
-func (ob *OrderBook) add(price float64, o *Order) {
+func (ob *OrderBook) PlaceLimitOrder(price float64, o *Order) {
 	var limit *Limit
 
 	if o.Bid {
@@ -104,12 +184,44 @@ func (ob *OrderBook) add(price float64, o *Order) {
 
 	if limit == nil {
 		limit = NewLimit(price)
+		limit.AddOrder(o)
+
 		if o.Bid {
-			ob.Asks = append(ob.Asks, limit)
+			ob.asks = append(ob.asks, limit)
 			ob.BidLimits[price] = limit
 		} else {
-			ob.Asks = append(ob.Bids, limit)
+			ob.asks = append(ob.bids, limit)
 			ob.AskLimits[price] = limit
 		}
 	}
+}
+
+func (ob *OrderBook) BidTotalVolume() float64 {
+	totalVolume := 0.0
+
+	for i := 0; i < len(ob.bids); i++ {
+		totalVolume += ob.bids[i].TotalVolume
+	}
+
+	return totalVolume
+}
+
+func (ob *OrderBook) AskTotalVolume() float64 {
+	totalVolume := 0.0
+
+	for i := 0; i < len(ob.asks); i++ {
+		totalVolume += ob.asks[i].TotalVolume
+	}
+
+	return totalVolume
+}
+
+func (ob *OrderBook) Asks() []*Limit {
+	sort.Sort(ByBestAsk{ob.asks})
+	return ob.asks
+}
+
+func (ob *OrderBook) Bids() []*Limit {
+	sort.Sort(ByBestBid{ob.bids})
+	return ob.bids
 }
--- orderbook_test.go
+++ orderbook_test.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
-// func TestOrderBook(t *testing.T) {
-
-// }
+func assert(t *testing.T, a, b any) {
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("%+v != %+v", a, b)
+	}
+}
 
 func TestLimit(t *testing.T) {
 	l := NewLimit(10_000)
@@ -26,10 +29,35 @@ func TestLimit(t *testing.T) {
 	fmt.Println(l)
 }
 
-func TestOrderBook(t *testing.T) {
+func TestPlaceLimitOrder(t *testing.T) {
 	ob := NewOrderBook()
-	buyOrder := NewOrder(true, 10)
-	ob.PlaceOrder(10_000, buyOrder)
 
-	fmt.Printf("%+v", ob.Bids[0])
+	sellOrderA := NewOrder(false, 10)
+	sellOrderB := NewOrder(false, 5)
+
+	ob.PlaceLimitOrder(10_000, sellOrderA)
+	ob.PlaceLimitOrder(9_000, sellOrderB)
+	assert(t, len(ob.asks), 1)
+}
+
+func TestPlaceMarketOrder(t *testing.T) {
+	ob := NewOrderBook()
+
+	sellOrder := NewOrder(false, 20)
+	ob.PlaceLimitOrder(10_000, sellOrder)
+
+	buyOrder := NewOrder(true, 10)
+	matches := ob.PlaceMarketOrder(buyOrder)
+
+	assert(t, len(matches), 1)
+	assert(t, len(ob.asks), 1)
+	assert(t, ob.AskTotalVolume(), 10.0)
+	assert(t, matches[0].Ask, 10.0)
+	assert(t, matches[0].Bid, buyOrder)
+	assert(t, matches[0].SizeFilled, 10.0)
+	assert(t, matches[0].Price, 10_000.0)
+
+	assert(t, buyOrder.isFilled(), true)
+
+	fmt.Printf("%+v", matches)
 }
